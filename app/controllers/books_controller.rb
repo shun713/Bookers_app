3fb36a0f7941600dc9beffb7a487eacc4751f120
shun713 <oizumi_shu18@yahoo.co.jp>
2021-07-23 08:56:51 +0000

    third-commit restart

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,24 @@
-class IndexController < ApplicationController
+class BooksController < ApplicationController
+  
   def index
+    @book = Book.all
+  end
+
+  def create
+    @book = book.new(book_params)
+    book.save
+    redirect_to book_show_path(book.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get "books/index"
-  get "books/show"
-  get "books/edit"
-  get "homes/top"
+  get "/" => "homes#top"
+  get "books" => "books#index" ,as:"books_index"
+  post "books" => "books#create"
+  get "books/:id" => "books#show" ,as:"book_show"
+  get "books/:id/edit" => "books#edit" ,as:"book_edit"
+  #patch "books/:id" => "books#update"
+  #delete "books/:id" => "books#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
