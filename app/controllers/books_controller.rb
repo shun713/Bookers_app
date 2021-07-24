0d5c0eabd501608cadf9f8d599abba04cbccc428
shun713 <oizumi_shu18@yahoo.co.jp>
2021-07-24 08:16:37 +0000

    7th-commit[finish]tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_show_path(@book.id), notice: "The Book was successfully saved."
+      redirect_to book_path(@book.id), notice: "The Book was successfully saved."
     else
       @books = Book.all
       render :index
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_show_path(@book.id), notice: "The Book was successfully updated."
+      redirect_to book_path(@book.id), notice: "The Book was successfully updated."
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get "books" => "books#index" ,as:"books"
   post "books" => "books#create"
-  get "books/:id" => "books#show" ,as:"book_show"
-  get "books/:id/edit" => "books#edit" ,as:"book_edit"
+  get "books/:id" => "books#show" ,as:"book"
+  get "books/:id/edit" => "books#edit" ,as:"edit_book"
   patch "books/:id" => "books#update"
-  delete "books/:id" => "books#destroy" ,as:"book_destroy"
+  delete "books/:id" => "books#destroy" ,as:"destroy_book"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
